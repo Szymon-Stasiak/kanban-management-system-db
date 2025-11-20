0633bfce2066e98bf1441c65faca2860d12b9459
Karol Leszynski <karol.leszynski1@gmail.com>
2025-11-20 16:27:13 +0100

    fixed bug in .sh script

--- db-run.sh
+++ db-run.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-cd "$(dirname "$0")/kanban-management-system-DB" || {
+cd "$(dirname "$0")" || {
   echo "Directory ./kanban-management-system-DB not found. Exiting."
   exit 1
 }
 
 echo "Stopping and removing all Docker containers and associated volumes..."
-docker-compose down -v
+docker compose down -v
 
 echo "Starting up the Docker containers..."
-docker-compose up
+docker compose up
